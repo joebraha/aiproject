228c4b8ca4432a0e452434689259054c43844f65
Joe Braha <jb7044@nyu.edu>
2023-04-24 11:37:05 -0400

    'mint autosave'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st  #Web App
 from transformers import pipeline
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 #title
@@ -12,19 +12,26 @@ def analyze(input, model):
 
 
 # load my fine-tuned model
-fine_tuned = None
+fine_tuned = "fine_tuned/______"
 
 
 #text insert
-input = st.text_area("insert text to be analyzed", value="Nice to see you today.", height=None, max_chars=None, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
+input = st.text_area("insert text to be analyzed", value="Nice to see you today.", 
+                     height=None, max_chars=None, key=None, help=None, on_change=None, 
+                     args=None, kwargs=None, placeholder=None, disabled=False, 
+                     label_visibility="visible")
 option = st.selectbox(
     'Choose a transformer model:',
     ('Default', 'Fine-Tuned' , 'Custom'))
 
 
 if option == 'Fine-Tuned':
-    model = TFAutoModelForSequenceClassification.from_pretrained(fine_tuned)
+    model = AutoModelForSequenceClassification.from_pretrained(fine_tuned)
     tokenizer = AutoTokenizer.from_pretrained(fine_tuned)
+    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+elif option == 'Roberta':
+    model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
+    tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
     classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
 else:
     classifier = pipeline('sentiment-analysis')
